f58e57dbcf65c08bd4228dc0980a2e4a8b16907f
MrSalei <ilia74311@yandex.by>
2022-09-09 23:31:54 +0300

    Chapter 10: Creating a macOS App

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by luqrri on 9.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
